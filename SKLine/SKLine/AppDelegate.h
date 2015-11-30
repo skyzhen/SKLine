ed38c1663ed43d28a370928667227bc973031fd7
lzb <skyzhenbin@sina.com>
2015-11-30 17:09:19 +0800

    create project

---
+++ SKLine/SKLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKLine
+//
+//  Created by lzb on 15/11/30.
+//  Copyright © 2015年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKLine/SKLine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SKLine
+//
+//  Created by lzb on 15/11/30.
+//  Copyright © 2015年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
